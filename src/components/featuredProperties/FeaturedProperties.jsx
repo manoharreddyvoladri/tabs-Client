ebdda2f59dbaa5a16b4f784d95a4a81bd7251b63
Manohar Reddy <manireddy2306@gmail.com>
2023-11-23 09:58:52 +0530

    Your commit message here

--- src/components/featuredProperties/FeaturedProperties.jsx
+++ src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,32 +1,75 @@
+// import useFetch from "../../hooks/useFetch";
+// import "./featuredProperties.css";
+
+// const FeaturedProperties = () => {
+//   const { data, loading, error } = useFetch("/hotels?featured=true&limit=4");
+// console.log(data);
+//   return (
+//     <div className="fp">
+//       {loading ? (
+//         "Loading"
+//       ) : (
+//         // <>
+//     {data.length>0 &&
+//                         {data.map((item) => (
+
+//                           <div className="fpItem" key={item._id}>
+//                             <img
+//                               src={item.photos[0]}
+//                               alt=""
+//                               className="fpImg"
+//                             />
+//                             <span className="fpName">{item.name}</span>
+//                             <span className="fpCity">{item.city}</span>
+//                             <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
+//                             {item.rating && <div className="fpRating">
+//                               <button>{item.rating}</button>
+//                               <span>Excellent</span>
+//                             </div>}
+//                           </div>
+//                         ))}
+//       }
+//         // </>
+//       )}
+//     </div>
+//   );
+// };
+
+// export default FeaturedProperties;
+
 import useFetch from "../../hooks/useFetch";
 import "./featuredProperties.css";
 
 const FeaturedProperties = () => {
   const { data, loading, error } = useFetch("/hotels?featured=true&limit=4");
-console.log(data);
+  console.log(data);
+
   return (
     <div className="fp">
       {loading ? (
         "Loading"
       ) : (
-        <>
-          {data.map((item) => (
+        // Check if data is an array and has elements
+        data && data.length > 0 ? (
+          // Use map inside JSX to render each item
+          data.map((item) => (
             <div className="fpItem" key={item._id}>
-              <img
-                src={item.photos[0]}
-                alt=""
-                className="fpImg"
-              />
+              <img src={item.photos[0]} alt="" className="fpImg" />
               <span className="fpName">{item.name}</span>
               <span className="fpCity">{item.city}</span>
               <span className="fpPrice">Starting from ${item.cheapestPrice}</span>
-              {item.rating && <div className="fpRating">
-                <button>{item.rating}</button>
-                <span>Excellent</span>
-              </div>}
+              {item.rating && (
+                <div className="fpRating">
+                  <button>{item.rating}</button>
+                  <span>Excellent</span>
+                </div>
+              )}
             </div>
-          ))}
-        </>
+          ))
+        ) : (
+          // Render a message if data is an empty array
+          "No featured properties available"
+        )
       )}
     </div>
   );
